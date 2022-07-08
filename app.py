6eceda43b733728b194307fa5a1d1d6fc5c82de7
AmauMaill <amaury.maill@gmail.com>
2022-07-08 20:54:27 +0200

    mvp app

---
+++ app.py
@@ -0,0 +1,16 @@
+from fastai.vision.all import load_learner
+import gradio as gr
+
+def predict(inp):
+    model = load_learner("./export.pkl")
+    labels = model.dls.vocab
+    prediction = model.predict(inp)
+    confidences = {labels[i]: float(prediction[2][i]) for i in range(2)}
+    return confidences
+
+gr.Interface(
+    fn=predict,
+    inputs=gr.inputs.Image(type="filepath"),
+    outputs=gr.outputs.Label(num_top_classes=2),
+    examples=["./data/kanelbullar.jpg", "./data/semlor.jpg"]
+).launch(inbrowser=True)
